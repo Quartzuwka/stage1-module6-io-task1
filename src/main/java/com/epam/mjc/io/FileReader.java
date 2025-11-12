6d1822010734be8f793dcccb4259a0ac2cd481a2
Quartzuwka <kvashnin.semion@yandex.ru>
2025-11-12 17:41:23 +0300

    done

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,53 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        List<String> list = new ArrayList<>();
+
+        try(InputStream in = new FileInputStream(file)) {
+
+            byte[] bytes = new byte[in.available()];
+            int counter = 0;
+            byte currentByte;
+            while ((currentByte = (byte) in.read()) != -1) {
+                bytes[counter++] = currentByte;
+            }
+
+            String stringValue = new String(bytes);
+
+            String[] lines = stringValue.split("\\r?\\n");
+
+
+
+            for (String line : lines) {
+                String[] lines2 = line.split(" ");
+                String s = lines2[1].trim();
+                list.add(s);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new Profile(
+                list.get(0),
+                Integer.parseInt(list.get(1)),
+                list.get(2),
+                Long.parseLong(list.get(3))
+        );
     }
+
+
+    public static void main(String[] args) throws IOException {
+
+
+    }
+
 }
